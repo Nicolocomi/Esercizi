838103be1f3486daa2ab4b84968ea901a504fd91
Nicolocomi <comignaninicolo@gmail.com>
2023-11-20 18:04:54 +0100

    Problemi in insertBucket

--- git_review.cpp
+++ git_review.cpp
@@ -12,15 +12,18 @@ struct dataStruct    // Gestisce l'array dinamico di interi
 };
 
 void ResizeArray (dataStruct& orig, int resize);
+void insertInBucket (dataStruct* bucket , int newItem);
+void removeFromBucket (dataStruct* bucket, int index);
 
 int main ()
 {
     dataStruct dinamic_array;
     int resize ;
+    int newItem ;
+    
     
     dinamic_array.data = new int [dinamic_array.size];
-    
-    
+
 
     for (int i = 0 ; i < dinamic_array.size ; i++)
     {
@@ -33,8 +36,14 @@ int main ()
     cout << "Inserisci nuova dimensione dell' array " << endl;
     cin >> resize;
     cout << "L'array ridimensionato e': " << endl;
+
     ResizeArray (dinamic_array,resize);
-    
+
+    cout << "Inserisci l'elemento che vuoi aggiungere : " << endl;
+    cin >> newItem;
+
+    insertInBucket (&dinamic_array,newItem);
+
     delete [] dinamic_array.data;
 
     return 0;
@@ -59,8 +68,24 @@ void ResizeArray(dataStruct& orig, int resize)
     delete[] orig.data; // Release memory of the old array
     orig.data = newData; // Update the pointer to the new array
     orig.size = resize;  // Update the size of the structure
-    orig.numData = resize; // Assuming you want to update the numData as well
-}
+    orig.numData = resize; // Assuming you want to update the numData as well 
 
+   if (resize > orig.size)
+    {
+        orig.numData += resize - orig.size;
+    }
+}
+void insertInBucket (dataStruct* bucket , int newItem)
+{
+    
+    if (bucket->size == bucket->numData)
+    {
+        ResizeArray(*bucket, bucket->size +1);
+    }
+    bucket->data[bucket->numData]= newItem;
+    bucket->numData++;
+    
+
+}
 
 
